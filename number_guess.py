760a1398b0d400801cfe436fd21ca8bf69d330af
Phil Ellsworth <philip.d.ellsworth@gmail.com>
2016-01-15 22:07:39 +0000

    added userName

--- number_guess.py
+++ number_guess.py
@@ -1,9 +1,13 @@
 import random, math
 f = 0
+# did IDLE change 'i' to 'f'? or did I just forget doing that?
 num = random.randint(1,100)
 tries = 0
-print "Guess a number between 0 and 100"
+userName = input("What's your name? ")
+print ("Ok", userName, ", guess a number between 0 and 100")
+# how to keep it from putting a space after userName?
 while f == 0:
+        
 	# convert to "input" from "raw_input" to work with 3.x
 	theGuess = int(input("Your guess: "))
 	if theGuess > num:
@@ -13,10 +17,13 @@ while f == 0:
 		tries = tries + 1
 		print("Your guess is too low.") 
 	if theGuess == num:
-		print("Great Job.")
+		print("Great job,", userName)# + userName
 		f = 1
 		
-		print('You got that in', tries, 'tries.') 
+		print('You got that in', tries, 'tries,', userName, ".") 
 	else:
-		print "Try again."
+		print ("Try again.")
+
+# how to save number of attempts per userName? Save to a spreadsheet?
+# Access that spreadsheet to compare averages?
 
